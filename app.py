4a111ebcc7c4f7a588db86d361279cf3b680b90f
jufehuva <91357991+jufehuva@users.noreply.github.com>
2022-06-26 13:06:22 -0500

    4

--- app.py
+++ app.py
@@ -28,6 +28,7 @@ localidades_df = pd.read_csv("./data-cleaned/localidades_properties.csv")
 with open("./data-cleaned/poligonos-localidades-min.json") as response:
     bogota_geojson = json.load(response)
 crime = pd.read_csv("./data-cleaned/Delitos_x_localidad.csv")
+attractions=pd.read_csv("./data-cleaned/number_of_touristic_attractions.csv")
 
 app.layout = html.Div([dcc.Location(id="url"), sidebar, content])
 
