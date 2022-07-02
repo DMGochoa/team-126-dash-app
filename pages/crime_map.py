cdab325f861c982a1eff97d9f730bcf047cf5462
Alberto Ramirez Velasquez <54113668+albertorave@users.noreply.github.com>
2022-07-02 01:30:11 -0500

    Delitos x Localidad x Horario
    Delitos x Localidad x Horario

--- pages/crime_map.py
+++ pages/crime_map.py
@@ -6,6 +6,7 @@ from random import randrange
 from dash import dcc, html
 import dash_bootstrap_components as dbc
 from dash import html
+from operator import xor
 
 def display():
     
@@ -15,6 +16,9 @@ def display():
             html.P(["La información recopilada para el análisis de los crimenes comprende los años 2021 y 2022 para la ciudad de Bogotá y fue exportada del portal de la ",html.A("Secretaria Distrital de Seguridad, Convivencia y Justicia",href="https://scj.gov.co/es/oficina-oaiee/estadisticas-mapas", target="_blank") , " en la consulta Delitos de alto impacto"]),
             html.Div([
                 dcc.Graph(figure=figura())
+            ]),
+            html.Div([
+                dcc.Graph(figure=barras())
             ])
             
         ],id="crime_map"
@@ -34,6 +38,18 @@ def figura():
                                mapbox_style="carto-positron", zoom=9)
     fig.update_geos(fitbounds="locations", visible=False)
     fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    return fig
+
+def barras():
+        
+    df_delitos = pd.read_csv("./data/Siedco_Datos_Detallados_Delitos.csv", sep=",")
+    df_delitos['Localidad']=df_delitos.Localidad.str[4:]
+    del_loc = df_delitos.groupby(['Localidad', 'Rango del Dia']).size().reset_index(name='Count')
+    del_loc['Localidad'] = del_loc['Localidad'].str.strip()
+    del_loc = del_loc[~ xor(del_loc['Localidad'] == 'SIN LOCALIZACION',del_loc['Localidad'] == 'SUMAPAZ')]
+    del_loc['Rango del Dia'] = pd.Categorical(del_loc['Rango del Dia'], ["MAÑANA", "TARDE", "NOCHE", "MADRUGADA"])
+    del_loc.sort_values("Rango del Dia", inplace=True)
+    fig = px.bar(del_loc, x="Localidad", y="Count", color="Rango del Dia", title="Delitos por Localidad segun el rango horario",  color_discrete_map={"MAÑANA": "#FBE426", "MADRUGADA": "#0D2A63", "NOCHE": "#1616A7","TARDE": "#DC3912"})
     return fig
 
 crime_map = display()
